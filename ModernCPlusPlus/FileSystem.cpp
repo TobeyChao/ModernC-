702812bc5acff118514115c0dbb229020c7cf3cc
TobeyChao <coolfire1314@outlook.com>
2020-05-14 19:37:43 +0800

    filesystem

--- ModernCPlusPlus/FileSystem.cpp
+++ ModernCPlusPlus/FileSystem.cpp
@@ -1,51 +1,60 @@
-////C++ : Check if given path is a file or directory using Boost & C++17 FileSystem Library
-//#include <experimental/filesystem>
-//#include <iostream>
-//
-//namespace filesys = std::experimental::filesystem;
-//
-///*
-//Check if given string path is of a Directory
-//*/
-//bool checkIfDirectory(std::string filePath)
-//{
-//	try {
-//		// Create a Path object from given path string
-//		filesys::path pathObj(filePath);
-//		// Check if path exists and is of a directory file
-//		if (filesys::exists(pathObj) && filesys::is_directory(pathObj))
-//			return true;
-//	}
-//	catch (filesys::filesystem_error & e)
-//	{
-//		std::cerr << e.what() << std::endl;
-//	}
-//	return false;
-//}
-//
-///*
-//	Check if given string path is of a file
-//*/
-//bool checkIfFIle(std::string filePath)
-//{
-//	try {
-//		// Create a Path object from given path string
-//		filesys::path pathObj(filePath);
-//		// Check if path exists and is of a regular file
-//		if (filesys::exists(pathObj) && filesys::is_regular_file(pathObj))
-//			return true;
-//	}
-//	catch (filesys::filesystem_error & e)
-//	{
-//		std::cerr << e.what() << std::endl;
-//	}
-//	return false;
-//}
-//
-//int main(int argc, char* argv[])
-//{
-//	auto a = checkIfFIle("G:/My Projects/VisualStudioProject/C++/ModernCPlusPlus/ModernCPlusPlus.sln");
-//
-//	std::cout << a;
-//	system("pause");
-//}
+//C++ : Check if given path is a file or directory using Boost & C++17 FileSystem Library
+#include <filesystem>
+#include <iostream>
+
+using namespace std::filesystem;
+
+/*
+Check if given string path is of a Directory
+*/
+bool checkIfDirectory(std::wstring filePath)
+{
+	try {
+		// Create a Path object from given path string
+		path pathObj(filePath);
+		// Check if path exists and is of a directory file
+		if (exists(pathObj) && is_directory(pathObj))
+			return true;
+	}
+	catch (filesystem_error& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+	return false;
+}
+
+/*
+	Check if given string path is of a file
+*/
+bool checkIfFIle(std::wstring filePath)
+{
+	try {
+		// Create a Path object from given path string
+		path pathObj(filePath);
+		// Check if path exists and is of a regular file
+		if (exists(pathObj) && is_regular_file(pathObj))
+			return true;
+	}
+	catch (filesystem_error& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+	return false;
+}
+
+int main(int argc, char* argv[])
+{
+	std::wstring filename = L"G:\\Projects\\VisualStudioProject\\ModernCPlusPlus\\ModernCPlusPlus.sln";
+	auto a = checkIfFIle(filename);
+	path pathObj(filename);
+	// 替换后缀
+	//pathObj.replace_extension();
+	// 替换文件名
+	//pathObj.replace_filename();
+	// 相对路径
+	std::cout << pathObj.relative_path() << std::endl;
+	std::cout << pathObj.relative_path() << std::endl;
+	std::cout << absolute(pathObj) << std::endl;
+	
+	return 0;
+}
